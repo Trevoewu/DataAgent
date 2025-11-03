528b911e4f35b0800636c8fdec5aa4dabd389ca5
hejianfei <452083691@qq.com>
2025-11-03 21:33:27 +0800

    init spring-boot 3.5.7

--- DataAgent-server/src/main/java/com/dataagent/DataAgentServerApplication.java
+++ DataAgent-server/src/main/java/com/dataagent/DataAgentServerApplication.java
@@ -1,13 +1,13 @@
-package com.example.dataagent;
+package com.dataagent;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DataAgentApplication {
+public class DataAgentServerApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(DataAgentApplication.class, args);
+        SpringApplication.run(DataAgentServerApplication.class, args);
     }
 
 }
---
+++ DataAgent-server/src/test/java/com/dataagent/DataAgentServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dataagent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataAgentServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
